88f1c384235e2ce14ba3212443de62eb1f14cdc0
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-28 17:13:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  minus = num2 - num1
+  minus
 end
 
 def division(num1, num2)
-
+  div = num2 / num1
+  div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
-
+  mod = num2 % num1
+  mod
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
